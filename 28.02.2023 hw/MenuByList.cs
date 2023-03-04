2efeff1449731d3977806be7df73ac05cd2d714b
TheSheleng <sheleng55@gmail.com>
2023-03-05 00:51:19 +0200

    Add project files.

---
+++ 28.02.2023 hw/MenuByList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBL
+{
+    internal class MenuByList
+    {
+        string name;
+        string[] items;
+        public MenuByList(string name, string fItem, params string[] items)
+        {
+            this.name = name;
+
+            this.items = new string[items.Length + 1];
+            this.items[0] = fItem;
+            for (int i = 0; i < items.Length; i++)
+            {
+                this.items[i + 1] = items[i];
+            }
+        }
+        void PrintMenu(int chise)
+        {
+            Console.WriteLine($"\n\t{name}:");
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine($"\t {(chise == i ? ">" : "")} {items[i]};");
+            }
+        }
+        public int GetChise()
+        {
+            int chise = 0;
+            while (true)
+            {
+                Console.Clear();
+                PrintMenu(chise);
+
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.UpArrow: chise = (chise == 0 ? items.Length - 1 : chise - 1); break;
+                    case ConsoleKey.DownArrow: chise = (chise == items.Length - 1 ? 0 : chise + 1); break;
+                    case ConsoleKey.Enter: Console.Clear(); return chise;
+                }
+            }
+        }
+    }
+}
---
+++ 28.02.2023 hw/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MBL;
+
+namespace _28._02._2023_hw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myDict = new Dictionary<string, string>();
+            myDict.Add("Ukraine", "Украина");
+            myDict.Add("Poland", "Польша");
+            myDict.Add("Germany", "Германия");
+            myDict.Add("USA", "США");
+
+            var ChoiceDict = new MenuByList("Choice Dictionary: ",
+                "English-Russian",
+                "Russian-English"
+            );
+
+            var sortedDict = new SortedDictionary<string, string>();
+            switch (ChoiceDict.GetChise())
+            {
+                case 0: foreach (var item in myDict) sortedDict.Add(item.Key, item.Value); break;
+                case 1: foreach (var item in myDict) sortedDict.Add(item.Value, item.Key); break;
+            }
+
+            Console.Write("\n\tWrite contry name: ");
+            string word = Console.ReadLine();
+            try { Console.WriteLine($"\tResult: {sortedDict[word]}"); }
+            catch { Console.WriteLine("\tIt's not in my data."); }
+
+            Console.ReadKey();
+        }
+    }
+}
